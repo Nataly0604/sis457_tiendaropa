b363cf8c2506a088db050356183abbed9819c096
Nataly0604 <natalytosubepinto06042002@gmail.com>
2024-11-05 18:55:00 -0400

    Creación de la base de datos y creación de las capas

---
+++ ddl_dml_LabSis457Tiendaropa.sql
@@ -0,0 +1,179 @@
+﻿﻿CREATE DATABASE Labsis457tiendaropa
+GO
+USE [master]
+GO
+CREATE LOGIN [usrtiendaropa] WITH PASSWORD = N'123456',
+ DEFAULT_DATABASE = [Labsis457tiendaropa],
+ CHECK_EXPIRATION = OFF,
+ CHECK_POLICY = ON
+GO
+USE [Labsis457tiendaropa]
+GO
+CREATE USER [usrtiendaropa] FOR LOGIN [usrtiendaropa]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrtiendaropa]
+GO
+CREATE TABLE Empleado(
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  cedulaIdentidad VARCHAR(12) NOT NULL,
+  nombres VARCHAR(30) NOT NULL,
+  primerApellido VARCHAR(30) NULL,
+  segundoApellido VARCHAR(30) NULL,
+  gmail VARCHAR(250) NOT NULL,
+  celular INT NOT NULL,
+  cargo VARCHAR(50) NOT NULL,
+  genero VARCHAR(1) NOT NULL
+);
+
+CREATE TABLE Proveedor (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  nombres varchar(30) not null,
+  documento VARCHAR(50) NOT NULL,
+  razonSocial VARCHAR(50) NOT NULL,
+  email VARCHAR(20) NOT NULL,
+  telefono VARCHAR(8) NOT NULL
+);  
+
+CREATE TABLE Cliente (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  documento VARCHAR(50) NOT NULL,
+  nombreCompleto VARCHAR(50) NOT NULL,
+  email VARCHAR(20) NOT NULL,
+  telefono VARCHAR(8) NOT NULL,
+  direcciON VARCHAR(30) NOT NULL
+); 
+
+CREATE TABLE Usuario (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idEmpleado INT NOT NULL,
+  usuario VARCHAR(15) NOT NULL,
+  clave VARCHAR(250) NOT NULL,
+  CONSTRAINT fk_Usuario_Empleado FOREIGN KEY (idEmpleado) REFERENCES Empleado(id)
+); 
+
+CREATE TABLE Compra (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idUsuario INT NOT NULL,
+  idProveedor INT NOT NULL,
+  numeroDocumento VARCHAR(50) NOT NULL,
+  montoTotal DECIMAL(10,2) NOT NULL,
+  CONSTRAINT fk_Compra_Usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+  CONSTRAINT fk_Compra_Proveedor FOREIGN KEY (idProveedor) REFERENCES Proveedor(id)
+); 
+
+CREATE TABLE Categoria (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  descripcion VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Producto (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idCategoria INT NOT NULL,
+  codigo VARCHAR(50) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  descripcion VARCHAR(100) NOT NULL,
+  stock INT NOT NULL DEFAULT 0,
+  precioVenta DECIMAL(10,2) DEFAULT 0,
+  CONSTRAINT fk_Producto_Categoria FOREIGN KEY (idCategoria) REFERENCES Categoria(id)
+);
+
+
+CREATE TABLE DetalleCompra (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idCompra INT NOT NULL,
+  idProducto INT NOT NULL,
+  precioCompra DECIMAL(10,2) DEFAULT 0,
+  cantidad INT NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  CONSTRAINT fk_DetalleCompra_Compra FOREIGN KEY (idCompra) REFERENCES Compra(id),
+  CONSTRAINT fk_DetalleCompra_Producto FOREIGN KEY (idProducto) REFERENCES Producto(id)
+); 
+
+CREATE TABLE Venta (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idUsuario INT NOT NULL,
+  tipoDocumento VARCHAR(50) NOT NULL,
+  numeroDocumento VARCHAR(50) NOT NULL,
+  documentoCliente VARCHAR(50) NOT NULL,
+  nombreCliente VARCHAR(100) NOT NULL,
+  montoPago DECIMAL(10,2) NOT NULL,
+  montoCambio DECIMAL(10,2) NOT NULL,
+  montoTotal DECIMAL(10,2) NOT NULL,
+  CONSTRAINT fk_Venta_Usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+); 
+
+CREATE TABLE DetalleVenta (
+  id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  idVenta INT NOT NULL,
+  idProducto INT NOT NULL,
+  precioVenta DECIMAL(10,2),
+  cantidad INT NOT NULL,
+  subTotal DECIMAL(10,2) NOT NULL,
+  numeroVenta INT NOT NULL,
+  CONSTRAINT fk_DetalleVenta_Venta FOREIGN KEY (idVenta) REFERENCES Venta(id),
+  CONSTRAINT fk_DetalleVenta_Producto FOREIGN KEY (idProducto) REFERENCES Producto(id)
+);
+
+ALTER TABLE DetalleVenta ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE DetalleVenta ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE DetalleVenta ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Venta ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Venta ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Venta ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE DetalleCompra ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE DetalleCompra ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE DetalleCompra ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Producto ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Producto ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Producto ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Categoria ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Categoria ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Categoria ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Compra ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Compra ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Compra ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Usuario ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Usuario ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Usuario ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Cliente ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Cliente ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Cliente ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Proveedor ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Proveedor ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Proveedor ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+
+ALTER TABLE Empleado ADD usuarioRegistro VARCHAR(50) NOT NULL DEFAULT SUSER_NAME();
+ALTER TABLE Empleado ADD fechaRegistro DATETIME NOT NULL DEFAULT GETDATE();
+ALTER TABLE Empleado ADD estado SMALLINT NOT NULL DEFAULT 1; -- -1: Eliminado, 0: Inactivo, 1: Activo
+GO
+
+SELECT * FROM Producto
+INSERT INTO Producto(idCategoria,codigo, nombre, descripcion, stock, precioVenta)
+  VALUES(1,'1Co', 'Colales Jandra','Algodón',100, 10),
+(2,'2Be', 'Bebetina Lucia','Algodón',100, 7),
+(3,'3Con', 'UNBRO','Poliestes',100, 12),
+(4,'4Bra', 'Jandra','Algodón',100, 10)
+
+ select * from Categoria
+
+  INSERT INTO Categoria(descripcion)
+VALUES('Colales'),
+('Bebetinas'),
+('Conjuntos'),
+('Boxer'),
+('Brasier');
+GO
+
+CREATE PROC paProductoListar @parametro VARCHAR(100)
+AS
+  SELECT * FROM Producto
+  WHERE estado<>-1 AND nombre LIKE '%'+REPLACE(@parametro, ' ', '%')+'%'
+  ORDER BY nombre;
