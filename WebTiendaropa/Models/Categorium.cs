712f4c8e13184ade6a31a5d5116fea4c8c5832fc
Nataly0604 <natalytosubepinto06042002@gmail.com>
2024-11-30 04:20:40 -0400

    categoria arregle inge

--- WebTiendaropa/Models/Categorium.cs
+++ WebTiendaropa/Models/Categorium.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebTiendaropa.Models;
 
-public partial class Categoria
+public partial class Categorium
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [StringLength(100, ErrorMessage = "La descripción no puede superar los 100 caracteres.")]
     public string Descripcion { get; set; } = null!;
 
     public string UsuarioRegistro { get; set; } = null!;
